e567343a0a3784c24d054fc47be14d7ee8fd852d
Jordi Carres <jcarres@mdsol.com>
2012-11-08 14:19:59 +0900

    add google analytics file

--- lib/dice_bag/templates/gems_checker.rb
+++ lib/dice_bag/templates/gems_checker.rb
@@ -1,6 +1,7 @@
 module DiceBag
   def self.needed_templates
     needed_templates = []
+    configured = Configuration.new
 
     if defined?(Dalli)
       needed_templates.push('dalli.yml.erb')
@@ -12,6 +13,10 @@ module DiceBag
 
     if defined?(AWS)
       needed_templates.push('aws.yml.erb')
+    end
+
+    if configured.google_analytics_id
+      needed_templates.push('google_analytics.yml.erb')
     end
 
     needed_templates
